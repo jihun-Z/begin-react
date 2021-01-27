e51f2e10780dcd9abf1f83c3dbeffc558a0337d4
jihun-z <wkftodrlswlgns82@gmail.com>
2021-01-28 01:15:48 +0900

    ContextAPI의 활용예제

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ContextSample from './ContextSample';
 //import *as serviceWorker from './serviceWorker';
 //import Counter from './Counter';
 
 ReactDOM.render(
 
-    <App />
+    <ContextSample />
 ,
   document.getElementById('root')
 );
